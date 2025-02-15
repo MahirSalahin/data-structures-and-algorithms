d005bb87a39acb500af9f491315a7c14fe8e6fe2
MahirSalahin <mahirsalahin01@gmail.com>
2025-02-15 14:11:37 +0600

    Add bucket sort implementation

---
+++ Algorithms/Sorting/bucket_sort.py
@@ -0,0 +1,20 @@
+def bucket_sort(my_list):
+    n = len(my_list)
+    mx = max(my_list)
+    bucket = [[] for _ in range(n)]
+    for i in range(n):
+        index = (my_list[i] * n) // (mx + 1)
+        bucket[index].append(my_list[i])
+    for i in range(n):
+        bucket[i].sort()
+    k = 0
+    for i in range(n):
+        for j in range(len(bucket[i])):
+            my_list[k] = bucket[i][j]
+            k += 1
+    return my_list
+
+
+if __name__ == '__main__':
+    a = [12, 1, 3, 124, 5, 986, 2, 6]
+    print(bucket_sort(a))
